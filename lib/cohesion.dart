dc75e028340d51cf8cff591154a08ce2d8fb085b
mggo96 <guedj.malkiel@gmail.com>
2023-11-29 16:22:18 +0100

    designe ameliore

---
+++ lib/cohesion.dart
@@ -0,0 +1,133 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Cohesion extends StatefulWidget {
+  const Cohesion({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Cohesion> createState() => _CohesionState();
+}
+
+List<String> employe = [
+  'Emma',
+  'Louis',
+  'Jade',
+  'Léo',
+  'Chloé',
+  'Lucas',
+  'Léa',
+  'Gabriel',
+  'Manon',
+  'Arthur',
+  'Lola',
+  'Raphaël',
+  'Inès',
+  'Hugo',
+  'Camille',
+  'Jules',
+  'Zoé',
+  'Liam',
+  'Louise',
+  'Paul',
+  'Anna',
+  'Nathan',
+  'Charlotte',
+  'Enzo',
+];
+String index_employe_un = "";
+String index_employe_deux = "";
+List<String> repOUcafe = ['cafe', "repas"];
+String resultRepas = "";
+
+class _CohesionState extends State<Cohesion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: Column(
+                  children: [
+                    Image.asset(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        'assets/images/imageResto.png'),
+                    const Text('pause cafe et repas',
+                        style: TextStyle(fontSize: 24.0)),
+                    const SizedBox(height: 20.0),
+                    ElevatedButton(
+                        onPressed: () {
+                          laBoucleAleatoire();
+                          choisirRepasOUcafe();
+                        },
+                        child: const Text(
+                          'choisi ton duo aleatoire',
+                          style: TextStyle(fontSize: 24.0),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 140),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Card(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        index_employe_un,
+                        style: TextStyle(fontSize: 24.0),
+                      ),
+                      Text(
+                        ' - ',
+                        style: TextStyle(fontSize: 24.0),
+                      ),
+                      Text(
+                        index_employe_deux,
+                        style: TextStyle(fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                  Text(
+                    resultRepas,
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void choisirRepasOUcafe() {
+    var rnd = Random();
+    resultRepas = repOUcafe[rnd.nextInt(repOUcafe.length)];
+  }
+
+  void laBoucleAleatoire() {
+    setState(() {
+      var rnd = Random();
+      do {
+        index_employe_un = employe[rnd.nextInt(employe.length)];
+        index_employe_deux = employe[rnd.nextInt(employe.length)];
+      } while (index_employe_un == index_employe_deux);
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+import 'cohesion.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,130 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'cohesion'),
+      home: const Cohesion(title: 'cohesion'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-List<String> employe = [
-  'Emma',
-  'Louis',
-  'Jade',
-  'Léo',
-  'Chloé',
-  'Lucas',
-  'Léa',
-  'Gabriel',
-  'Manon',
-  'Arthur',
-  'Lola',
-  'Raphaël',
-  'Inès',
-  'Hugo',
-  'Camille',
-  'Jules',
-  'Zoé',
-  'Liam',
-  'Louise',
-  'Paul',
-  'Anna',
-  'Nathan',
-  'Charlotte',
-  'Enzo',
-];
-String index_employe_un = "";
-String index_employe_deux = "";
-List<String> repOUcafe = ['cafe', "repas"];
-String resultRepas = "";
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          Center(
-            child: Card(
-              elevation: 5,
-              child: Column(
-                children: [
-                  Image.asset(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      'assets/images/imageResto.png'),
-                  const Text('pause cafe et repas',
-                      style: TextStyle(fontSize: 24.0)),
-                  const SizedBox(height: 20.0),
-                  ElevatedButton(
-                      onPressed: () {
-                        laBoucleAleatoire();
-                        choisirRepasOUcafe();
-                      },
-                      child: const Text(
-                        'choisi ton duo aleatoire',
-                        style: TextStyle(fontSize: 24.0),
-                      )),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 40),
-          Card(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      index_employe_un,
-                      style: TextStyle(fontSize: 24.0),
-                    ),
-                    Text(
-                      ' - ',
-                      style: TextStyle(fontSize: 24.0),
-                    ),
-                    Text(
-                      index_employe_deux,
-                      style: TextStyle(fontSize: 24.0),
-                    )
-                  ],
-                ),
-                Text(
-                  resultRepas,
-                  style: TextStyle(fontSize: 24.0),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void choisirRepasOUcafe() {
-    var rnd = Random();
-    resultRepas = repOUcafe[rnd.nextInt(repOUcafe.length)];
-  }
-
-  void laBoucleAleatoire() {
-    setState(() {
-      var rnd = Random();
-      do {
-        index_employe_un = employe[rnd.nextInt(employe.length)];
-        index_employe_deux = employe[rnd.nextInt(employe.length)];
-      } while (index_employe_un == index_employe_deux);
-    });
   }
 }
